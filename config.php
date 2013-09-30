ebf7ac45471009f9c64aa27585f41d8cfb63e775
newnewyear <newyearsevenydny@gmail.com>
2013-10-01 00:15:30 +0400

    Revert "SERVER_NAME"
    This reverts commit 92c530a15c171eecaf9ede680e30051392476369.

--- config.php
+++ config.php
@@ -1,10 +1,5 @@
 <?
 	$api_id = 3903026;  // ID приложения
 	$secret_key = '6uqVbacpZzJf4Cg7NKTe';  // ID аккаунта
-	//$server_name =  'http://'.$_SERVER["SERVER_NAME"].'/';
-
-	$patch = trim(substr(substr(__FILE__, strlen(realpath($_SERVER['DOCUMENT_ROOT']))), 0, - strlen(basename(__FILE__))), DIRECTORY_SEPARATOR);
-	$patch = str_replace('\\', '/', $patch); 
-	if (empty($server_name))
-		$server_name = trim('http://'.$_SERVER["SERVER_NAME"].'/'.$patch, "/").'/';
+	$server_name =  'http://localhost/';
 ?>
